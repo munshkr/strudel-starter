6e9556e0c3a87c1fbf74763772c15dc898a189ed
Damián Silvani <munshkr@gmail.com>
2022-06-18 13:42:27 -0300

    Delay loadWebdirt and Scheduler creation until user clicks Start button
    This solves the interaction issue with audio context creation.

--- main.js
+++ main.js
@@ -13,44 +13,51 @@ await evalScope(
   // import other strudel packages here
 ); // add strudel to eval scope
 
-const audioContext = getAudioContext();
-const latency = 0.2;
-
-// load default samples + init webdirt
-loadWebDirt({
-  audioContext,
-  latency,
-  sampleMapUrl: 'https://strudel.tidalcycles.org/EmuSP12.json',
-  sampleFolder: 'https://strudel.tidalcycles.org/EmuSP12/',
-});
-
-// the scheduler will query the pattern within the given interval
-const scheduler = new Scheduler({
-  audioContext,
-  interval: 0.1,
-  latency,
-  onEvent: (hap) => {
-    /*     const delta = hap.whole.begin - audioContext.currentTime;
-    console.log('delta', delta); */
-    // when using .osc or .webdirt, each hap will have context.onTrigger set
-    // if no onTrigger is set, try to play hap.value as frequency with a cheap oscillator
-    if (!hap.context.onTrigger && typeof (hap.value) === "number") {
-      // console.log('e', e.show());
-      const oscillator = audioContext.createOscillator();
-      const master = audioContext.createGain();
-      master.gain.value = 0.1;
-      master.connect(audioContext.destination);
-      oscillator.type = 'sawtooth';
-      oscillator.frequency.value = hap.value;
-      oscillator.connect(master);
-      oscillator.start(hap.whole.begin);
-      oscillator.stop(hap.whole.end);
-    }
-  },
-});
+let scheduler;
+let initialized = false;
 
 // eval + start on button click
 document.getElementById('start').addEventListener('click', async () => {
+  if (!initialized) {
+    const audioContext = getAudioContext();
+    const latency = 0.2;
+
+    // load default samples + init webdirt
+    loadWebDirt({
+      audioContext,
+      latency,
+      sampleMapUrl: 'https://strudel.tidalcycles.org/EmuSP12.json',
+      sampleFolder: 'https://strudel.tidalcycles.org/EmuSP12/',
+    });
+
+    // the scheduler will query the pattern within the given interval
+    scheduler = new Scheduler({
+      audioContext,
+      interval: 0.1,
+      latency,
+      onEvent: (hap) => {
+        /*     const delta = hap.whole.begin - audioContext.currentTime;
+        console.log('delta', delta); */
+        // when using .osc or .webdirt, each hap will have context.onTrigger set
+        // if no onTrigger is set, try to play hap.value as frequency with a cheap oscillator
+        if (!hap.context.onTrigger && typeof (hap.value) === "number") {
+          // console.log('e', e.show());
+          const oscillator = audioContext.createOscillator();
+          const master = audioContext.createGain();
+          master.gain.value = 0.1;
+          master.connect(audioContext.destination);
+          oscillator.type = 'sawtooth';
+          oscillator.frequency.value = hap.value;
+          oscillator.connect(master);
+          oscillator.start(hap.whole.begin);
+          oscillator.stop(hap.whole.end);
+        }
+      },
+    });
+
+    initialized = true;
+  }
+
   const { pattern } = await evaluate(
     // 's("bd sd").osc()', // need to run sclang + osc server (npm run osc in strudel root)
     `stack(
